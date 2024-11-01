c08d8e0e66ca10247d5c43ab632be79691b7995b
Ovidijus <ovidijus.e@gmail.com>
2024-11-01 17:51:58 +0200

    init

---
+++ src/test/java/org/example/company/OpenBankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.company;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenBankingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
